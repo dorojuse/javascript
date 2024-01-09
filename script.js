22fb7ca36e1343ac7b81c49696d0fe2b58141a57
dorojuse <doroseitz@yahoo.com>
2024-01-09 10:01:21 +0100

    practice

---
+++ script.js
@@ -0,0 +1,50 @@
+// single line comment
+
+/* das ist ein multi line comment */
+
+// string data type
+console.log("Hello World");
+
+//arithmetic operators
+console.log(1 + 5);
+console.log(567 + 19375);
+console.log(5 - 1);
+console.log(6 * 6);
+console.log(99 / 10);
+
+//comparison operators
+console.log(1 == 3);
+console.log(1 <= 1);
+console.log(1 != 2);
+
+//strict equal operator
+console.log(1 === 1);
+console.log(1 !== 2);
+
+//logical operators
+console.log(2 > 1 && 8 < 7);
+console.log(5 === 5 || 4 < 2);
+console.log(!(1 === 1));
+
+//grouping operator
+console.log((2 + 5) * 7);
+
+//concatenation operator
+console.log("My " + "name " + "is " + "Doro ");
+
+//Coding-Task:
+console.log("D" + "o" + "r" + "o" + "t" + "h" + "e" + "a");
+
+console.log("Hallo");
+
+console.log("That doesn't work");
+
+console.log(50 + 2 >= 52);
+
+console.log(!"333" == 333);
+
+console.log((600 + 510) / 2);
+
+console.log(2 + 2);
+
+console.log(25 / 5 === 5 && 50 / 10 === 5);
