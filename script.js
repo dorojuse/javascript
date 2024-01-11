c405b84ecb76f84cfb86eb010e2aab5b6858ac9e
dorojuse <doroseitz@yahoo.com>
2024-01-11 14:14:06 +0100

    sixth pratice

--- script.js
+++ script.js
@@ -195,3 +195,57 @@ console.log(oldYoung(86));
 // result should be elder person
 
 console.log(oldYoung("Doro"));
+
+//06)
+function oddNumbers(a, b) {
+  let store = ""; //quasi ein Container, um die Werte zu storen
+  for (let index = a; index <= b; index++) {
+    if (index % 2 !== 0) {
+      store += index; //hier wächst store um ein index
+      if (index !== b && index !== b - 1) {
+        store += ","; //hier kommt hinter jeder Zahl ein Komma, außer der letzten, weil sie b oder b-1 ist
+      }
+    }
+  }
+  if (a < 0 || b < 0) {
+    return "invalid";
+  }
+  return store; //die Variable let store kommt dann bei console.log raus
+}
+
+console.log(oddNumbers(0, 4));
+// result should be: 1,3
+
+console.log(oddNumbers(10, 33));
+// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+
+console.log(oddNumbers(9, 12));
+// result should be: 9,11
+
+function charCount(word, character) {
+  let output = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (character.length > 1) {
+      return "Error";
+    }
+    if (word[i] === character) {
+      output++;
+    }
+  }
+  return output;
+}
+console.log(charCount("hello", "l"));
+// result should be: 2
+
+console.log(charCount("mama", "m"));
+// result should be: 2
+
+console.log(charCount("Resümee", "e"));
+// result should be: 3
+
+console.log(charCount("Resümee", "eh"));
+// result should be: 3
+
+console.log(charCount("Resümee", "k"));
+// result should be: 3
