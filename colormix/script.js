f852d020711247fdc45bca687cc266430125f8c6
dorojuse <doroseitz@yahoo.com>
2024-01-30 16:21:41 +0100

    working color mixer

--- colormix/script.js
+++ colormix/script.js
@@ -1,16 +1,21 @@
 const colorResult = document.body.querySelector("p");
+const main = document.body.querySelector("main");
+const header = document.body.querySelector("header");
 let red = document.body.querySelector(".red");
 let green = document.body.querySelector(".green");
 let blue = document.body.querySelector(".blue");
 
 function render() {
-  red.value;
-  green.value;
-  blue.value;
   colorResult.textContent =
-    "rgb (" + red.value + "," + green.value + "," + blue.value + ")";
+    "rgb(" + red.value + ", " + green.value + ", " + blue.value + ")";
 }
+render();
 
 //Event Listener
+header.addEventListener("change", () => {
+  let colorName = colorResult.textContent;
+  main.style.backgroundColor = colorName;
+  render();
+});
 
 render();
