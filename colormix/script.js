0a31138ccb53a0bbba40852917dbda8d21422b01
dorojuse <doroseitz@yahoo.com>
2024-01-31 14:46:34 +0100

    add state and localStorage

--- colormix/script.js
+++ colormix/script.js
@@ -5,18 +5,45 @@ let red = document.body.querySelector(".red");
 let green = document.body.querySelector(".green");
 let blue = document.body.querySelector(".blue");
 
-function render() {
-  red.value;
-  green.value;
-  blue.value;
-  colorResult.textContent =
-    "rgb(" + red.value + "," + green.value + "," + blue.value + ")";
-  render();
+let state = {
+  redValue: 0,
+  greenValue: 0,
+  blueValue: 0,
+};
+
+const savedState = localStorage.getItem("color-mixer-state");
+if (savedState) {
+  state = JSON.parse(savedState);
 }
-//Event Listener
-header.addEventListener("click", () => {
+
+function render() {
+  localStorage.setItem("color-mixer-state", JSON.stringify(state));
+
+  colorResult.textContent =
+    "rgb(" +
+    state.redValue +
+    "," +
+    state.greenValue +
+    "," +
+    state.blueValue +
+    ")";
   main.style.backgroundColor =
-    "rgb(" + red.value + "," + green.value + "," + blue.value + ")";
+    "rgb(" +
+    state.redValue +
+    "," +
+    state.greenValue +
+    "," +
+    state.blueValue +
+    ")";
+  red.value = state.redValue;
+  green.value = state.greenValue;
+  blue.value = state.blueValue;
+}
+
+header.addEventListener("input", () => {
+  state.redValue = red.value;
+  state.greenValue = green.value;
+  state.blueValue = blue.value;
   render();
 });
 
