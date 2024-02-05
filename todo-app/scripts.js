1d59d819f88e41df8761d80f7cda54343960a669
dorojuse <doroseitz@yahoo.com>
2024-02-05 17:10:41 +0100

    add filter function

--- todo-app/scripts.js
+++ todo-app/scripts.js
@@ -1,11 +1,36 @@
 const addForm = document.getElementById("add-form");
+const list = document.querySelector("#list");
+const descriptionInput = document.querySelector("#description");
+const removebtn = document.querySelector(".removebtn");
+const openbtn = document.getElementById("open");
+const donebtn = document.getElementById("done");
+const allbtn = document.getElementById("all");
+const filterForm = document.getElementById("filter-form");
 
 let state = {
-  todos: [{ description: "Learn CSS", done: true }, {}, {}],
+  todos: [
+    {
+      id: 1,
+      description: "Learn CSS",
+      done: true,
+    },
+    {
+      id: 2,
+      description: "Learn HTML",
+      done: false,
+    },
+  ],
+  filter: "all", // open, done
 };
 
+const savedState = localStorage.getItem("todo-state");
+if (savedState) {
+  state = JSON.parse(savedState);
+}
+
 function renderTodos() {
-  const list = document.querySelector("#list");
+  localStorage.setItem("todo-state", JSON.stringify(state));
+
   list.innerHTML = "";
 
   state.todos.forEach((todo) => {
@@ -15,27 +40,60 @@ function renderTodos() {
     checkbox.type = "checkbox";
     checkbox.checked = todo.done;
 
-    checkbox.addEventListener("change", (e) => {
-      const newTodoDoneState = e.target.checked;
-      todo.done = newTodoDoneState;
+    checkbox.addEventListener("change", () => {
+      todo.done = !todo.done;
+
+      renderTodos();
     });
 
-    todoLi.appendChild(checkbox);
-
     const todoText = document.createTextNode(todo.description);
-    todoLi.append(todoText);
+    todoLi.append(checkbox, todoText);
 
     list.appendChild(todoLi);
   });
 }
 
-renderTodos();
-
 addForm.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("Submit");
-  const newDescription = document.querySelector("#description").value;
-  let newObject = { description: newDescription, done: false };
-  state.todos.push(newObject);
+
+  const inputText = descriptionInput.value;
+  const newDescription = inputText.trim();
+  const generatedId = Date.now().toString();
+
+  state.todos.push({
+    id: generatedId,
+    description: newDescription,
+    done: false,
+  });
+
+  //removeDuplicates?
+
+  renderTodos();
+  descriptionInput.value = "";
+});
+
+/*removebtn.addEventListener("click", () => {
+  state.todos.forEach((todo) => {
+    if (todo.done) {
+      todo.innerHTML = "";
+    }
+  });
+});*/
+
+allbtn.addEventListener("click", () => {
+  state.filter = "all";
   renderTodos();
 });
+
+openbtn.addEventListener("click", () => {
+  state.filter = "open";
+  renderTodos();
+});
+
+donebtn.addEventListener("click", () => {
+  state.filter = "done";
+  renderTodos();
+});
+
+renderTodos();
